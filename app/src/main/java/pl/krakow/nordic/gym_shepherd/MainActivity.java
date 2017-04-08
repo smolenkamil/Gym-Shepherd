280e49d05704a46ee62cdfe92b6bc78413ce1322
Kamil <pl.smolen.kamil@gmail.com>
2017-04-08 22:43:19 +0200

    xxx

--- app/src/main/java/pl/krakow/nordic/gym_shepherd/MainActivity.java
+++ app/src/main/java/pl/krakow/nordic/gym_shepherd/MainActivity.java
@@ -11,6 +11,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    //kamilBranch
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
