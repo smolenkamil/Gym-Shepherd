92457b7e83d66e912c12feb574fc325dc3089e97
Doberman96 <dobosz.ma@interia.pl>
2017-04-09 14:37:06 +0200

    In MainActivity.java - floating button AddTrainingFAB is now pointing to activity AddTrainingActivity
    -----------------------------------------------------------------------------
    
    In AddTrainingActivity.xml
    
    - changed the name of the activity to: 'Dodaj trening' in AppManifest.xml
    
    - added an EditText 'trainingName' with default value set to 'Nazwa treningu'
    
    - programatically added EditTexts to enter exercise names, including:
    	- add unlimited EditTexts
    	- added hint (default text) on each of the EditTexts
    	- request focus on newly made EditText
    
    TODO: working on Scrolling Issues, so that when you make new EditTexts, a newly made EditText should be visible over the keyboard
    
    TORETHINK: Should there be a 'delete' button near to every exercise?

--- app/src/main/java/pl/krakow/nordic/gym_shepherd/AddTrainingActivity.java
+++ app/src/main/java/pl/krakow/nordic/gym_shepherd/AddTrainingActivity.java
@@ -2,12 +2,40 @@ package pl.krakow.nordic.gym_shepherd;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 public class AddTrainingActivity extends AppCompatActivity {
+    public ScrollView scrollView;
+    public LinearLayout linearLayout;
+    public Button addExerciseButton;
+    public EditText trainingName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_training);
+        linearLayout = (LinearLayout) findViewById(R.id.linearLayoutInnerId);
+        addExerciseButton = (Button) findViewById(R.id.addExerciseButton);
+        scrollView = (ScrollView) findViewById(R.id.scrollViewId);
+        trainingName = (EditText) findViewById(R.id.trainingName);
+        addExerciseButton.performClick();
+        trainingName.requestFocus();
+    }
+
+    public void addExerciseButtonClick(View view) {
+        EditText exercise = new EditText(this);
+        exercise.setHint("Ćwiczenie, np. Wyciskanie 3x12");
+        exercise.setLines(1);
+        exercise.setMaxLines(1);
+        exercise.setInputType(4001); // 61 - code for textPersonName
+        linearLayout.removeView(addExerciseButton);
+        linearLayout.addView(exercise);
+        linearLayout.addView(addExerciseButton);
+        exercise.requestFocus();
+        // TODO: Ustawić scrollView aby przycisk addExerciseButton był zawsze widoczny nad klawiaturą
     }
 }
