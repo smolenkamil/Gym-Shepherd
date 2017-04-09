213459f564a2c17066c02d80a897d5e7e53e0059
Doberman96 <dobosz.ma@interia.pl>
2017-04-09 11:00:25 +0200

    Initial commit of AddTrainingActivity

---
+++ app/src/main/java/pl/krakow/nordic/gym_shepherd/AddTrainingActivity.java
@@ -0,0 +1,13 @@
+package pl.krakow.nordic.gym_shepherd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddTrainingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_training);
+    }
+}
