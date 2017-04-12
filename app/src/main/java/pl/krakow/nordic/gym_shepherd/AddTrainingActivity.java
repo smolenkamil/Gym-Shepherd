9c0a6e89e5ebf4717cb9950af8426a173f111974
Doberman96 <dobosz.ma@interia.pl>
2017-04-12 13:09:46 +0200

    Improvements in AddTrainingActivity: - fixed errors concerning exercise Id - improved logical functionality of Save and Discard buttons - deleted unnecessary code

--- app/src/main/java/pl/krakow/nordic/gym_shepherd/AddTrainingActivity.java
+++ app/src/main/java/pl/krakow/nordic/gym_shepherd/AddTrainingActivity.java
@@ -4,13 +4,10 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.annotation.IdRes;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
-import android.util.Log;
-import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -19,17 +16,19 @@ import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.Toast;
 
-import static android.R.attr.id;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddTrainingActivity extends AppCompatActivity {
-    private ScrollView scrollView;
     private LinearLayout linearLayout;
     private Button addExerciseButton;
+    private Button saveTrainingButton;
+    private Button discardTrainingButton;
     private EditText trainingName;
-    private EditText exercise;
-    //private Button saveTrainingButton;
-    private int exerciseId;
+    private List<EditText> exercisesArray;
+
     private int trainingId;
+
     private SharedPreferences data;
     private SharedPreferences.Editor dataExecutor;
 
@@ -48,53 +47,52 @@ public class AddTrainingActivity extends AppCompatActivity {
     private void setTrainingId() {
         data = getSharedPreferences("different_values", Context.MODE_PRIVATE);
         trainingId = data.getInt("trainingId", 0);
-
     }
 
     private void init() {
-        exerciseId = 1;
         linearLayout = (LinearLayout) findViewById(R.id.linearLayoutInnerId);
         addExerciseButton = (Button) findViewById(R.id.addExerciseButton);
-        scrollView = (ScrollView) findViewById(R.id.scrollViewId);
+        saveTrainingButton = (Button) findViewById(R.id.saveTrainingButton);
+        discardTrainingButton = (Button) findViewById(R.id.discardTrainingButton);
+        exercisesArray = new ArrayList<>();
         trainingName = (EditText) findViewById(R.id.trainingName);
         data = getSharedPreferences("training" + trainingId, Context.MODE_PRIVATE);
         dataExecutor = data.edit();
     }
 
     public void addExerciseButtonClick(View view) {
-        exercise = new EditText(this);
-        exercise.setId(exerciseId); //set id of exercise
+        EditText exercise = new EditText(this);
         exercise.setHint("Ćwiczenie, np. Wyciskanie 3x12");
         exercise.setMaxLines(1);
         exercise.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_CAP_SENTENCES); // 61 - code for textPersonName
+
         linearLayout.removeView(addExerciseButton);
         linearLayout.addView(exercise);
         linearLayout.addView(addExerciseButton);
+
         exercise.requestFocus();
-        exerciseId++;
+        exercisesArray.add(exercise);
         // TODO: Ustawić scrollView aby przycisk addExerciseButton był zawsze widoczny nad klawiaturą
     }
 
 
-    public void saveExerciseButtonClick(View view) {
-        try {
-            dataExecutor.putString("trainingname", trainingName.getText().toString());
-            for (int i = 1; i < exerciseId; i++) {
-                EditText currentEx = (EditText) findViewById(i);
-                dataExecutor.putString("exercise" + i, currentEx.getText().toString());
-            }
-            dataExecutor.apply();
-            Toast.makeText(this, "Saved!", Toast.LENGTH_SHORT).show();
-        } catch (Exception e) {
-            Log.i("throw", e.toString());
+    public void saveTrainingButtonClick(View view) {
+        dataExecutor.putString("trainingname", trainingName.getText().toString());
+        int i = 1;
+        for (EditText exercise : exercisesArray) {
+            dataExecutor.putString("exercise" + i, exercise.getText().toString());
+            i++;
         }
+        dataExecutor.apply();
+        Toast.makeText(this, "Saved!", Toast.LENGTH_SHORT).show();
         saveTrainingId();
         Intent intent = new Intent(AddTrainingActivity.this, MainActivity.class);
         startActivity(intent);
     }
 
-    public void discardExerciseButtonClick(View view) {
-        //TODO: to be continued
+    public void discardTrainingButtonClick(View view) {
+        Intent intent = new Intent(AddTrainingActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 
     private void saveTrainingId() {
@@ -105,33 +103,19 @@ public class AddTrainingActivity extends AppCompatActivity {
         dataExecutor.apply();
     }
 
-
-    //ZAPIS - do przeklejenia
-    public void showMeEverything(View view) {
-        trainingName.setText(data.getString("trainingname", "empty"));
-        for (int i = 1; i< exerciseId ; i++){
-
-            EditText currentEx = (EditText) findViewById(i);
-            currentEx.setText(data.getString("exercise"+i, "empty"));
-        }
-
-        Toast.makeText(this, "Yeah!", Toast.LENGTH_SHORT).show();
-    }
-
     @Override
     public void onBackPressed() {
         new AlertDialog.Builder(this)
-                .setTitle("Zapis")
+                .setTitle("Zapisz")
                 .setMessage("Zapisać wprowadzone zmiany?")
-                .setPositiveButton("Zapisz i wyjdź", new DialogInterface.OnClickListener() {
+                .setPositiveButton("Zapisz", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
-                        saveExerciseButtonClick(scrollView);
+                        saveTrainingButton.performClick();
                     }
                 })
                 .setNegativeButton("Nie", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
-                        Intent intent = new Intent(AddTrainingActivity.this, MainActivity.class);
-                        startActivity(intent);
+                        discardTrainingButton.performClick();
                     }
                 })
                 .setIcon(android.R.drawable.ic_dialog_alert)
