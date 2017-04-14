6cc51d17db9aae2f747db453470fe86179fa0fa9
Kamil <pl.smolen.kamil@gmail.com>
2017-04-14 22:50:54 +0200

    phone error repair

--- app/src/main/java/pl/krakow/nordic/gym_shepherd/MainActivity.java
+++ app/src/main/java/pl/krakow/nordic/gym_shepherd/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.Menu;
@@ -27,9 +28,15 @@ public class MainActivity extends AppCompatActivity {
     private int trainingId;
     private SharedPreferences data;
     private Button training;
+    private Button exitButton;
+    private static boolean dontSplash = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (dontSplash == false) {
+            startActivity(new Intent(MainActivity.this, SplashScreen.class));
+            dontSplash = true;
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -37,13 +44,17 @@ public class MainActivity extends AppCompatActivity {
 
         linearLayout = (LinearLayout) findViewById(R.id.linearLayoutMain);
         scrollView = (ScrollView) findViewById(R.id.scrollViewMain);
-        getTrainingId();
+        try {
+            getTrainingId();
+        } catch (Exception e) {
+            Log.i("error", e.getMessage().toString());
+        }
         createTrainingButtons(scrollView);
 
     }
 
     private void createTrainingButtons(View view) {
-        for(int i=0; i<trainingId; i++) {
+        for (int i = 0; i < trainingId; i++) {
             //TODO: jesli pusty rekord 'trainingId' na wypadek usunięcia rekordu
             data = getSharedPreferences("training" + i, Context.MODE_PRIVATE);
             training = new Button(this);
@@ -53,24 +64,39 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(getBaseContext(), "ok", Toast.LENGTH_SHORT).show();
                 }
             });
-            LinearLayout.LayoutParams parameter =  new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.MATCH_PARENT);
+            LinearLayout.LayoutParams parameter = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.MATCH_PARENT);
             parameter.gravity = Gravity.CENTER;
             parameter.width = LinearLayout.LayoutParams.WRAP_CONTENT;
             training.setLayoutParams(parameter);
             training.setText(data.getString("trainingname", "empty"));
             linearLayout.addView(training);
+        }
+        try {
             training.requestFocus();
+        } catch (Exception e) {
+            Log.i("error", e.getMessage().toString());
         }
     }
 
     private void getTrainingId() {
-        data = getSharedPreferences("different_values" , Context.MODE_PRIVATE);
+        data = getSharedPreferences("different_values", Context.MODE_PRIVATE);
         trainingId = data.getInt("trainingId", 0);
+    }
+
+    @Override
+    protected void onStop() {
+        dontSplash = true;
+        super.onStop();
     }
 
     public void addTrainingFABClick(View view) {
         Intent intent = new Intent(this, AddTrainingActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
     }
 
     @Override
@@ -95,3 +121,4 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 }
+
